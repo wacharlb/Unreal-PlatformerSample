dac3daaf9cd1480d8e48bf15a1e0a39e8b498417
Charles Beall <wacharlb@gmail.com>
2022-05-10 13:56:05 -0700

    Initial Commit

---
+++ Source/PlatformerSample/PlatformerSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlatformerSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMERSAMPLE_API APlatformerSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
